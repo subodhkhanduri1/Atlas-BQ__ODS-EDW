71e5d2ca89535136e58a65022c75ca31f0faa3ed
Subodh Khanduri <subodhkhanduri1@users.noreply.github.com>
2025-07-20 18:25:32 +0530

    Update ODStoEDW_MoviesCDCSource.java

--- src/main/java/ods_edw/ODStoEDW_MoviesCDCSource.java
+++ src/main/java/ods_edw/ODStoEDW_MoviesCDCSource.java
@@ -34,12 +34,43 @@ public class ODStoEDW_MoviesCDCSource {
         Block<ChangeStreamDocument<Document>> printBlock = new Block<ChangeStreamDocument<Document>>() {
             @Override
             public void apply(final ChangeStreamDocument<Document> changeStreamDocument) {
-                System.out.println(changeStreamDocument.getFullDocument().toJson());
 
+                String messageData;
+
+                // Determine the operation type and construct the message accordingly
+                if (changeStreamDocument.getOperationType() == OperationType.UPDATE) {
+                    System.out.println("Update operation detected for document ID: " + changeStreamDocument.getDocumentKey());
+                    // For updates, you might want to send the updated fields (updateDescription)
+                    // along with the full document, or just the full document after the update.
+                    // Here, we'll send the full document for simplicity, but you could customize.
+                    if (changeStreamDocument.getFullDocument() != null) {
+                        messageData = changeStreamDocument.getFullDocument().toJson();
+                    } else {
+                        // If full document is not available for update (e.g., if configured not to send it)
+                        // you might want to send the updated fields from getUpdateDescription()
+                        messageData = "{\"_id\":" + changeStreamDocument.getDocumentKey().toJson() + ", \"operationType\":\"UPDATE\", \"updatedFields\":" + changeStreamDocument.getUpdateDescription().getUpdatedFields().toJson() + "}";
+                        return;
+                    }
+                } else if (changeStreamDocument.getOperationType() == OperationType.INSERT ||
+                           changeStreamDocument.getOperationType() == OperationType.REPLACE) {
+                    System.out.println("Insert/Replace operation detected for document ID: " + changeStreamDocument.getDocumentKey());
+                    messageData = changeStreamDocument.getFullDocument().toJson();
+                } else if (changeStreamDocument.getOperationType() == OperationType.DELETE) {
+                    System.out.println("Delete operation detected for document ID: " + changeStreamDocument.getDocumentKey());
+                    // For deletes, getFullDocument() will be null, so send the document key.
+                    messageData = "{\"_id\":" + changeStreamDocument.getDocumentKey().toJson() + ", \"operationType\":\"DELETE\"}";
+                    System.out.println("Publishing message: " + messageData);
+                    return;
+                } else {
+                    // For other operation types like DROP, RENAME, etc.
+                    System.out.println("Other operation detected: " + changeStreamDocument.getOperationType());
+                    messageData = changeStreamDocument.toString(); // Send the entire change stream document as a string
+                    return;
+                }
 
                 try {
-                    System.out.println(changeStreamDocument.getFullDocument());
-                    ByteString data = ByteString.copyFromUtf8(changeStreamDocument.getFullDocument().toJson());
+                    System.out.println("Publishing message: " + messageData);
+                    ByteString data = ByteString.copyFromUtf8(messageData);
                     PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
                             .setData(data)
                             .build();
@@ -57,7 +88,9 @@ public class ODStoEDW_MoviesCDCSource {
         /**
          * Change stream listener
          */
-        collection.watch().forEach(printBlock);
+        collection.watch()
+            .fullDocument(FullDocument.UPDATE_LOOKUP)
+            .forEach(printBlock);
 
     }
 }
